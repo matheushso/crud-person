26448b5c2c3926d141fe9b34e51b90d7cc2f5748
matheushso <matheushso99@hotmail.com>
2023-03-24 13:15:19 -0300

    first commit

---
+++ src/main/java/com/person/crud/CrudPersonApplication.java
@@ -0,0 +1,12 @@
+package com.person.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPersonApplication.class, args);
+	}
+}
